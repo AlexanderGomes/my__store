cddb26e4484f4546b94a16fecd4218a3981a9bb5
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-05-27 16:30:15 -0700

    done

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -4,7 +4,7 @@ import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 
 import { TiDeleteOutline } from 'react-icons/ti';
 import toast from 'react-hot-toast';
 import {useStateContext} from '../context/StateContext'
-import {urlFor} from '../LIB/client'
+import {urlFor} from '../lib/client'
 import getStripe from '../lib/getStripe';
 
 const Cart = () => {
--- components/Products.jsx
+++ components/Products.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import {urlFor} from '../LIB/client'
+import {urlFor} from '../lib/client'
 
 const Products = ({product: {image, name, slug, price}}) => {
   return (
