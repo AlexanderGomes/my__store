80007fa33ceac49b523d49a8f5c80c314ae7120c
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-05-28 15:05:20 -0700

    done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,34 +6,20 @@ const Hero = () => {
     <div className='hero__main'>
     <div className='hero__text'>
        
-       <motion.div
-      animate={{ x: [-200, 0], opacity: [0, 1] }}
-      transition={{ duration: 2 }}
-       >
       <h2>We have the<span className='hero__span'> products</span></h2>
-       </motion.div>
 
-      <motion.div
-          animate={{ x: [200, 0], opacity: [0, 1] }}
-      transition={{ duration: 3 }}
-      >
+
+    
       <h2 className='hero__text__h2'><span className='hero__span'>You</span> didn't know</h2>
-      </motion.div>
 
-   <motion.div
-       animate={{ x: [-200, 0], opacity: [0, 1] }}
-      transition={{ duration: 3.30 }}
-   >
+
+  
       <h2 className='hero__text__h22'>You  <span className='hero__span'>need</span></h2>
-   </motion.div>
+
      
     </div>
     <div className='hero__img'>
-    <motion.div
-        animate={{ x: [-200, 0], opacity: [0, 1] }}
-      transition={{ duration: 1 }}
-    >
-    </motion.div>
+
     </div>
       <div className='move__img'>
         <img className='img__hero' src={'/hero.webp'} alt="" />
